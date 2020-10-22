e8b5f3120cd099e597e694341a23f03d2489f716
Serdar Gurbanmyradov <serdargurbanmyradow1996@gmail.com>
2020-10-22 11:00:40 +0300

    Выберите несколько задолженностей, чтобы погасить их одновременно(2-я часть)

--- app/js/main.js
+++ app/js/main.js
@@ -1,28 +1,30 @@
-import { mediaQueries } from "./mediaQueries/mediaQueries";
-import { popupsInit } from "./modules/popups";
-import { menuInit } from "./modules/menu";
-import { inputsMaskInit } from "./modules/inputsMask";
-import { datePickerInit } from "./modules/datePicker";
-import { customSelectInit } from "./modules/select";
-import { validationInit } from "./modules/validation";
-import { slider } from "./modules/sliders";
-import { smoothScroll } from "./modules/smoothScroll";
-import { smartScrollingInit } from "./modules/smartScrolling";
-import { innToggleInit } from "./modules/innToggle";
-import { debtCheckFormPos } from "./modules/debtCheckFormPos";
-import { checkTravelBanSteps } from "./modules/checkTravelBanSteps";
+import { mediaQueries } from './mediaQueries/mediaQueries'
+import { popupsInit } from './modules/popups'
+import { menuInit } from './modules/menu'
+import { inputsMaskInit } from './modules/inputsMask'
+import { datePickerInit } from './modules/datePicker'
+import { customSelectInit } from './modules/select'
+import { validationInit } from './modules/validation'
+import { slider } from './modules/sliders'
+import { smoothScroll } from './modules/smoothScroll'
+import { smartScrollingInit } from './modules/smartScrolling'
+import { innToggleInit } from './modules/innToggle'
+import { debtCheckFormPos } from './modules/debtCheckFormPos'
+import { checkTravelBanSteps } from './modules/checkTravelBanSteps'
+import { chooseCheckboxexInit } from './modules/chooseCheckbox'
 
 // initFunctions
-mediaQueries();
-menuInit();
-inputsMaskInit();
-popupsInit();
-datePickerInit();
-customSelectInit();
-validationInit();
-slider();
-smoothScroll();
-smartScrollingInit();
-innToggleInit();
-debtCheckFormPos();
-checkTravelBanSteps();
+mediaQueries()
+menuInit()
+inputsMaskInit()
+popupsInit()
+datePickerInit()
+customSelectInit()
+validationInit()
+slider()
+smoothScroll()
+smartScrollingInit()
+innToggleInit()
+debtCheckFormPos()
+checkTravelBanSteps()
+chooseCheckboxexInit()
