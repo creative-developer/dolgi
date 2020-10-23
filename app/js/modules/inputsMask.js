11414b8a8dd97d8332a3380ce6203bee2db4499c
Serdar Gurbanmyradov <serdargurbanmyradow1996@gmail.com>
2020-10-23 14:39:05 +0300

    Проверить запрет на выезд за границу онлайн (Валидация)

--- app/js/modules/inputsMask.js
+++ app/js/modules/inputsMask.js
@@ -4,4 +4,7 @@ export const inputsMaskInit = () => {
   $('input[name="birthDate"]').inputmask('99.99.9999')
   $('input[name="passportSeries"]').inputmask('99 99 999999')
   $('input[name="inn"]').inputmask('999999999999')
+  $('input[name="driverCert"]').inputmask('9{2}A{2}9{6}')
+  $('input[name="stateSign"]').inputmask('A{1}9{3}A{2}9{2,3}')
+  $('input[name="vehicleCertificate"]').inputmask('9{10}')
 }
--- app/js/modules/validation.js
+++ app/js/modules/validation.js
@@ -1,3 +1,5 @@
+import { inputs } from './validationMethods'
+
 // Validate all form in site
 export let validator
 
@@ -48,6 +50,18 @@ jQuery.validator.setDefaults({
       required: true,
       inn: true,
     },
+    driverCert: {
+      required: true,
+      driverCert: true,
+    },
+    stateSign: {
+      required: true,
+      stateSign: true,
+    },
+    vehicleCertificate: {
+      required: true,
+      vehicleCertificate: true,
+    },
     pay_method: {
       required: true,
     },
@@ -68,33 +82,10 @@ jQuery.validator.setDefaults({
   },
 })
 
-$.validator.addMethod('fullname', function (value, element) {
-  return (
-    this.optional(element) ||
-    /^[А-Яа-я][а-я]{2,}(?:\s+[А-Яа-я][а-я]{2,})?(?:\s+[А-Яа-я][а-я]{2,})(?:\s+)*\r?$/.test(value)
-  )
-})
-
-$.validator.addMethod('birthDate', function (value, element) {
-  return this.optional(element) || /^[0-9\.]{10}?$/.test(value)
-})
-
-$.validator.addMethod('cyryllic', function (value, element) {
-  return this.optional(element) || /^[а-я\s?-\s?А-Я]*\s?\s?$/.test(value)
-})
-
-$.validator.addMethod('passportSeries', function (value, element) {
-  return this.optional(element) || /^[0-9\s]{11,12}?$/.test(value)
-})
-
-$.validator.addMethod('inn', function (value, element) {
-  return this.optional(element) || /^[0-9]{12}?$/.test(value)
-})
-
-$.validator.addMethod('phone', function (value, element) {
-  return (
-    this.optional(element) || /^.. [(][0-9]{3}[)] [0-9]{3}[-][0-9]{2}[-][0-9]{2}\s?$/.test(value)
-  )
+inputs.forEach(({ selector, regexp }) => {
+  $.validator.addMethod(selector, function (value, element) {
+    return this.optional(element) || regexp.test(value)
+  })
 })
 
 export const validationInit = () => {
