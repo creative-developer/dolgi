75bb482509f060e14926fa2eae14f25aafc56c66
Хатам Союнов <ksoyunov@i-neti.ru>
2020-10-24 14:07:09 +0300

    Адаптив - новости

--- app/js/mediaQueries/mediaQueries.js
+++ app/js/mediaQueries/mediaQueries.js
@@ -1,11 +1,11 @@
 const breakpoints = {
-  xl: '(max-width: 1199px)',
-  lg: '(max-width: 991px)',
-  md: '(max-width: 767px)',
-  sm: '(max-width: 575px)',
-  xsm: '(max-width: 374px)',
-}
-const MQ = $.mq.action
+  xl: "(max-width: 1199px)",
+  lg: "(max-width: 991px)",
+  md: "(max-width: 767px)",
+  sm: "(max-width: 575px)",
+  xsm: "(max-width: 374px)",
+};
+const MQ = $.mq.action;
 export const mediaQueriesInit = () => {
   MQ(
     breakpoints.lg,
@@ -14,6 +14,26 @@ export const mediaQueriesInit = () => {
     },
     () => {
       // $('.header__col--right').append($('.nav--header'))
+    }
+  );
+
+  MQ(
+    breakpoints.sm,
+    () => {
+      // news filters horizontal scroll width calc
+      $(window).on("load", function () {
+        let filtersWidth = 5;
+
+        $(".news-filters__item").each(function (index, item) {
+          filtersWidth += $(item).outerWidth(true);
+        });
+
+        $(".news-filters__list").css("width", filtersWidth);
+      });
     },
-  )
-}
+    () => {
+      // news filters horizontal scroll width calc
+      $(".news-filters__list").css("width", "auto");
+    }
+  );
+};
